049d20abf4380f67a686a062d9a8cc6f7cbc9539
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-09-19 20:31:42 -0600

    Ejercicio a
    Se identificó cuáles variables no serán modificadas para declararlas como constante, dado a que no es necesario declarar una variable para la impresión, solo se omitió el paso de la variable “saludo” y se creó un console.log concatenando texto con las variables.

---
+++ Practica1/a.js
@@ -0,0 +1,17 @@
+//Problema
+// var nombre = "Armando";
+// var edad = 25;
+
+// nombre = "Ana Maria";
+
+// var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+
+//Declaración de la variable y la constante
+
+let nombre = "Armando"; // Es let porque se le asigará otro valor
+const edad = 25; //Es const porque no habrá modiciación
+
+nombre = "Ana Maria";//reasignación del nombre
+
+console.log("hola", nombre, "Tienes", edad, "años.");//Impresión en consola
