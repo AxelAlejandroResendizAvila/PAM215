74db15ff3428c2166a2f75361d11f75e367a8adc
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-09-19 20:33:46 -0600

    Ejercicio c
    Conociendo la sintaxis de las funciones arrow, se creó la función saludoPersonalisado que recibirá dos parametros, nombre y edad. La función retornará una cadena de texto con los parametros concatenados y se  imprimirá en consola mandando a llamar la función dandole dos parametros, Axel y 20 que son el nombre y la edad.

---
+++ Practica1/c.js
@@ -0,0 +1,6 @@
+// Problema: Saludo personalizado
+
+saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+//Impresión en consola
+console.log(saludoPersonalizado("Axel", 20));
