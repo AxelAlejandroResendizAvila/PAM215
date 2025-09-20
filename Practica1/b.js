4688a1cc17c57d837628768b09bab2296c196e78
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-09-19 20:33:17 -0600

    ejercicio b
    Se declaró la función llamada “cuadrado” y recibirá un parametro llamado numero, la función hará una operación para obtener el cuadrado de un numero y al imprimir solo se concatenó el valor a imprimir con saltos de línea para evitar varios console.log.

---
+++ Practica1/b.js
@@ -0,0 +1,8 @@
+//Problema
+
+// function cuadrado(numero) {
+//     return numero * numero;
+// }
+
+cuadrado = numero => numero * numero;//función flecha
+console.log(cuadrado(5) + "\n" + cuadrado(10)+ "\n" + cuadrado(15));//Impresión en consola del resultado de la función cuadrado con dos valores diferentes
