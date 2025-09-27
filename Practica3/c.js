d4303a9f9fead2d8715a8305f711e831e4aac13e
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-09-26 21:43:20 -0600

    ejercicio c
    Simulando una API creamos la función que retornará una promesa que tardará 5 segundos en resolverse.
    Posteriormente en una función asíncrona con un try catch mandamos a llamar a la función con un await y si no se resuelve retornará el error.

---
+++ Practica3/c.js
@@ -0,0 +1,18 @@
+function simularPeticionAPI() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+                resolve("Datos recibidos correctamente");
+                console.log("Datos recibidos correctamente");
+          
+        }, 5000);
+    });
+}
+
+async function obtenerDatos() {
+    try{
+        await simularPeticionAPI();    
+    }catch(e){
+        console.log(e);
+    }
+}
+obtenerDatos();
