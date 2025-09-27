12fad8a6b4af818197f9d4943e4b2787115c7157
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-09-26 21:42:53 -0600

    ejercicio b
    Primero debemos crear la promesa que será retornada  con un resolve y un reject, si el usuario no es “Admin”, el acceso será denegado, lo validamos con un if y retornará un resolve de ser verdadero o reject de ser falso.
    Se manda a llamar la función dos veces con casos distintos para ver su funcionamiento en casos distintos con try y catch.

---
+++ Practica3/b.js
@@ -0,0 +1,17 @@
+function verificarUsuario(usuario){
+    return new Promise((resolve, reject) => {
+        if(usuario === "Admin"){
+            resolve("Acceso Concedido");
+        }else{
+            reject("Acceso denegado");
+        }
+    });
+}
+
+verificarUsuario("Admin")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+verificarUsuario("Axel")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
