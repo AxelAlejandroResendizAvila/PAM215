576e2e0eb1af443d13e3af8a78da095cb0770583
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-10-10 20:10:50 -0600

    Practica 5
    Cambios del programa para agregar componentes y modificar las propiedades con los usos de estado

--- intro/App.js
+++ intro/App.js
@@ -1,6 +1,11 @@
-import { View, Text } from "react-native";
+//1. imports: Zona de importaciones
+import { StyleSheet, View, Text, Button } from "react-native";
+import React, {useState} from "react";
 
+//2. Main: Zona de componentes
 export default function App() {
+  //Variable y una función
+  const[contador, setContador]=useState(0);
   return (
     <View
       style={{
@@ -9,7 +14,12 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Hola Mundo React Native</Text>
+      <Text>Contador: {contador}</Text>
+      <Button title='Incrementar' onPress={()=>setContador(contador+1)}></Button>
+      <Button title='Quitar' onPress={()=>setContador(contador-1)}></Button>
+      <Button title='Reiniciar' onPress={()=>setContador(contador-contador)}></Button>
     </View>
   );
 }
+
+//3. Styles: Zona de estilos y posicionamiento
