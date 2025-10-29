d3c1fb379eb7f42646423b07f33f9bf55a5816a1
AxelAlejandroResendizAvila <124049803@upq.edu.mx>
2025-10-28 20:29:09 -0600

    ActivityIndicator
    Con esta práctica podemos aplicar los conocimientos adquiridos en el curso para crear vista de carga de una manera estética y funcional.

--- intro/screens/IndicadorScreens.js
+++ intro/screens/IndicadorScreens.js
@@ -1,14 +1,60 @@
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, Button, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
 
 export default function IndicadorScreen() {
+  const[ loading, setLoading ] = useState(false);
+
+  const startLoading = () => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
+  };
+
+  if(loading) {
+    return(
+      <View style={styles.container}>
+        <View style={styles.loaderContainer}>
+          <ActivityIndicator
+            size='large'
+            color='#ff3434ff'
+            animating={true}
+            hidesWhenStopped={true}
+          />
+          <Text style={styles.text}>Cargando...</Text>
+        </View>
+      </View>
+    )
+  }
+
+
 
     return (
-      <View>
-        <Text>Proximamente por Leonel Emiliano</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>ActivityIndicator</Text>
+        <Button color={'red'} title='Carga de datos' onPress={startLoading}/>
       </View>
     )
 
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#32322dff',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: 'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'white',
+  },
+  loaderContainer: {
+    alignItems: 'center',
+  }
+})
